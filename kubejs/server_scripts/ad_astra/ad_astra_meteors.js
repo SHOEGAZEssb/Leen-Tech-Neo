ac974683c2ec5ceb1a742f1ce042a04823b087fb
Winterspelleen <Winterspell@gmx.net>
2026-01-26 17:57:43 +0100

    Alex Update Test Push

--- kubejs/server_scripts/ad_astra/ad_astra_meteors.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.highPriorityData(event => {
-  console.log("Patching ad astra meteorite spawn...")
-
-  event.addJson('ad_astra:tags/worldgen/biome/has_structure/meteor_biomes', {
-    "replace": true,
-    "values": [
-    ]
-  })
-})
