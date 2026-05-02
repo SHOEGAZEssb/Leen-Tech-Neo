ee44fcad6d3b594711050f9716fcbc87a8817c44
Winterspelleen <Winterspell@gmx.net>
2026-05-02 23:10:45 +0200

    Update jei_cleanup_client.js

--- kubejs/client_scripts/jei_cleanup_client.js
+++ kubejs/client_scripts/jei_cleanup_client.js
@@ -19,37 +19,8 @@ JEIEvents.hideItems(event => {
     global.itemsToHideInJei.forEach(id => event.hide(id))
   }
 
-  // aether
-  hideWildcard(event, "aether:boss_doorway*")
-  hideWildcard(event, "aether:*angelic_stone", ["aether:angelic_stone", "aether:light_angelic_stone"])
-  hideWildcard(event, "aether:*carved_stone", ["aether:carved_stone"])
-  hideWildcard(event, "aether:*hellfire_stone", ["aether:hellfire_stone", "aether:light_hellfire_stone"])
-  hideWildcard(event, "aether:*sentry_stone", ["aether:sentry_stone"])
-  event.hide("aether:aether_portal_frame")
-  event.hide("aether:chest_mimic")
-
-  // aether redux
-  hideWildcard(event, "aether_redux:boss_doorway*")
-  hideWildcard(event, "aether_redux:*carved_base", ["aether_redux:carved_base"])
-  hideWildcard(event, "aether_redux:*carved_pillar", ["aether_redux:carved_pillar"])
-  hideWildcard(event, "aether_redux:*sentry_base", ["aether_redux:sentry_base"])
-  hideWildcard(event, "aether_redux:*sentry_pillar", ["aether_redux:sentry_pillar"])
-  event.hide("aether_redux:locked_polished_sentrite")
-  event.hide("aether_redux:locked_runelight")
-
-  // aether: lost content
-  hideWildcard(event, "lost_aether_content:*gale_stone", ["lost_aether_content:gale_stone"])
-
   // better archeology
   event.hide("betterarcheology:infested_mud_bricks")
-
-  // blue skies
-  event.hide("blue_skies:everbright_portal")
-  event.hide("blue_skies:everdawn_portal")
-
-  // deep aether
-  hideWildcard(event, "deep_aether:*nimbus_stone", ["deep_aether:nimbus_stone", "deep_aether:light_nimbus_stone"])
-  hideWildcard(event, "deep_aether:*nimbus_pillar", ["deep_aether:nimbus_pillar", "deep_aether:light_nimbus_pillar"])
 
   // display delight
   hideWildcard(event, "displaydelight:*")
@@ -84,20 +55,4 @@ JEIEvents.hideItems(event => {
 
   // species
   event.hide("species:spectralibur_pedestal")
-
-  // twilight forest
-  hideWildcard(event, "twilightforest:*boss_spawner")
-  hideWildcard(event, "twilightforest:*miniature_structure")
-  event.hide("twilightforest:auroralized_glass")
-  event.hide("twilightforest:brittle_potion_flask")
-  event.hide("twilightforest:candelabra")
-  event.hide("twilightforest:cinder_furnace")
-  event.hide("twilightforest:cinder_log")
-  event.hide("twilightforest:cinder_wood")
-  event.hide("twilightforest:cube_of_annihilation")
-  event.hide("twilightforest:greater_potion_flask")
-  event.hide("twilightforest:keepsake_casket")
-  event.hide("twilightforest:ore_meter")
-  event.hide("twilightforest:slider")
-  event.hide("twilightforest:wrought_iron_fence")
 })
